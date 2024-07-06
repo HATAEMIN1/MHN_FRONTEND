85b64b12e23af61b8664998e6babded54e374d9f
ked1125 <ked1125@naver.com>
2024-07-06 17:40:31 +0900

    헤더/네브바 마무리/아이콘파일넣어놨음

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ButtonBlack from "../../components/ButtonBlack";
 
 function Header({ ...props }) {
     const text1 = props.text1;
+    const navigate = useNavigate();
     return (
         <>
-            {/* <div className="bg-slate-400">{props.title}</div> */}
-            <div className="bg-slate-400 top-0 z-50 w-[100%]  top-0 left-0 right-0 absolute h-[65px] px-[16px] flex items-center justify-between">
-                <Link to="/user/main">
-                    <div className="h-[100%] flex items-center">
-                        <img
-                            src="/assets/images/backIcon.svg"
-                            alt=""
-                            className="w-[30px] h-[30px] "
-                        />
-                    </div>
-                </Link>
+            <div className="bg-white top-0 z-50 w-[100%]  top-0 left-0 right-0 absolute h-[65px] px-[16px] flex items-center justify-between border-b">
+                <div className="h-[100%] flex items-center">
+                    <img
+                        src="/assets/images/backIcon.svg"
+                        alt=""
+                        className="w-[30px] h-[30px] cursor-pointer"
+                        onClick={() => {
+                            navigate(-1);
+                        }}
+                    />
+                </div>
                 <div className="text-[20px]">{props.title}</div>
                 {props.button ? (
                     // <button>{props.button}</button>
--- src/layouts/nav/NavBar.js
+++ src/layouts/nav/NavBar.js
@@ -1,9 +1,61 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function NavBar() {
+    const location = useLocation();
+    const navMenu = [
+        {
+            title: "홈",
+            route: "/user/main",
+            grayIcon: "home_gray",
+            blackIcon: "home_black",
+        },
+        {
+            title: "진료기록",
+            route: "/chart/:userId",
+            grayIcon: "medicalHistory_gray",
+            blackIcon: "medicalHistory_black",
+        },
+        {
+            title: "병원찾기",
+            route: "/hospital/map",
+            grayIcon: "search_gray",
+            blackIcon: "search_black",
+        },
+
+        {
+            title: "게시판",
+            route: "/board",
+            grayIcon: "board_gray",
+            blackIcon: "board_black",
+        },
+        {
+            title: "마이페이지",
+            route: "/account",
+            grayIcon: "account_gray",
+            blackIcon: "account_black",
+        },
+    ];
     return (
-        <div className="bg-slate-400 bottom-0 z-50 fixed w-[100%] left-0 right-0 absolute h-[65px] px-[16px">
-            Navbar
+        <div className="bg-white bottom-0 z-50 fixed w-[100%] left-0 right-0 absolute h-[65px] px-[16px] border-t flex">
+            <ul className="flex justify-around items-center w-full">
+                {navMenu.map((item, idx) => {
+                    const isActive = location.pathname === item.route;
+                    console.log(location.pathname);
+
+                    return (
+                        <Link to={item.route} key={idx}>
+                            <li className="text-center mini">
+                                <img
+                                    src={`/assets/images/${isActive ? item.blackIcon : item.grayIcon}.svg`}
+                                    className="m-auto"
+                                />
+                                {item.title}
+                            </li>
+                        </Link>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
