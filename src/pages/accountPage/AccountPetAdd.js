8477c804fc4970a8141246715c9b1f52271620b4
jjh <wogml220723@naver.com>
2024-07-08 09:40:07 +0900

    아이콘,폰트 수정

--- src/pages/accountPage/AccountPetAdd.js
+++ src/pages/accountPage/AccountPetAdd.js
@@ -1,7 +1,37 @@
 import React from "react";
-//
-function AccountPetAdd(props) {
-    return <div>AccountPetAdd</div>;
+import Header from "../../layouts/header/Header";
+import NavBar from "../../layouts/nav/NavBar";
+import Icons from "../../assets/Icons";
+
+function AccountPetAdd() {
+    return (
+        <>
+            <Header title="펫 등록" />
+            <div className="flex">
+                <img src={Icons.pets} />
+                <h2>펫 정보를 입력해주세요</h2>
+            </div>
+            <div>
+                <p className="flex">
+                    <span>이름</span>
+                    <input></input>
+                </p>
+                <p className="flex">
+                    <span>종</span>
+                    <select></select>
+                    <img src={Icons.dog} />
+                    <p>강아지</p>
+                    <img src={Icons.cat} />
+                    <p>고양이</p>
+                </p>
+                <p className="flex">
+                    <span>생일</span>
+                    <p>2000.00.00</p>
+                </p>
+            </div>
+            <NavBar />
+        </>
+    );
 }
 
 export default AccountPetAdd;
--- src/pages/accountPage/AccountPetList.js
+++ src/pages/accountPage/AccountPetList.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../layouts/header/Header";
+import NavBar from "../../layouts/nav/NavBar";
 
-function AccountPetList(props) {
-    return <div>AccountPetList</div>;
+function AccountPetList() {
+    return (
+        <>
+            <Header title="펫 리스트" />
+            <div>AccountPetList</div>
+            <NavBar />
+        </>
+    );
 }
 
 export default AccountPetList;
