bed0293a85350e9eea3d7bbdffbf22d97c528cd1
ked1125 <ked1125@naver.com>
2024-07-10 14:52:24 +0900

    백업

--- src/components/button/ButtonBlack.js
+++ src/components/button/ButtonBlack.js
@@ -6,7 +6,7 @@ function ButtonBlack({ ...props }) {
             {props.width ? (
                 <>
                     <button
-                        className="bg-primary-300 rounded-[4px] px-[16px] text-white text-center"
+                        className="bg-primary-300 rounded-[8px] px-[16px] text-white text-center"
                         style={{ width: props.width, height: props.height }}
                         // 대부분 width="100%" height="45px"로 맞춰주시면 됩니다
                         onClick={props.handleClick}
@@ -17,7 +17,7 @@ function ButtonBlack({ ...props }) {
             ) : (
                 <>
                     <button
-                        className="bg-primary-300 rounded-[4px] px-[16px] py-[4px] text-white text-center"
+                        className="bg-primary-300 rounded-[8px] px-[16px] py-[4px] text-white text-center"
                         onClick={props.handleClick}
                     >
                         {props.text1}
