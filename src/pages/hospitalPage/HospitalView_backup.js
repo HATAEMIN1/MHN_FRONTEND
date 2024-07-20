bbea8ea18b2d5648dd980978bb79cc1842585fa0
ked1125 <ked1125@naver.com>
2024-07-20 19:54:18 +0900

    북마크 등록중.. delete 백엔드 해야함

--- src/pages/hospitalPage/HospitalView_backup.js
+++ src/pages/hospitalPage/HospitalView_backup.js
@@ -1,14 +1,11 @@
-// 내일 해야할 일 - 포스트 요청으로 연결해주고, get해온 응답값 setBookmarkState이 null이면 bookmark가 false값이도록, 있으면 bookmark가 true값이도록 조정
 import React, { useEffect, useState } from "react";
 import Header from "../../layouts/header/Header";
 import NavBar from "../../layouts/nav/NavBar";
 import { Link, useParams } from "react-router-dom";
 import KakaoMapView from "../../components/kakaomap/KakaoMapView";
-import axiosInstance from "../../utils/axios";
 
 function HospitalView() {
     const [bookmark, setBookmark] = useState(false);
-    const [bookmarkState, setBookmarkState] = useState(null);
     function handleOnclick() {
         setBookmark(!bookmark);
         console.log(bookmark);
@@ -19,28 +16,6 @@ function HospitalView() {
     useEffect(() => {
         console.log("부모 hospitalInfo::", hospitalInfo);
     }, [hospitalInfo]);
-
-    const addBMK = () => {
-        console.log("북마크 추가할거임");
-    };
-    const deleteBMK = () => {
-        console.log("북마크 삭제할거임");
-    };
-    const fetchBMK = async () => {
-        try {
-            const response = await axiosInstance.get(
-                // `/hospitals/bmk?hospitalId=${hpId}&memberId=${나중에 여기 로그인유저값 들어가야함}`
-                `/hospitals/bmk?hospitalId=${hpId}&memberId=1`
-            );
-            setBookmarkState(response.data);
-            console.log("bmk상태는::", response.data);
-        } catch (error) {
-            console.error("병원 정보를 가져오는 중 오류 발생:", error);
-        }
-    };
-    useEffect(() => {
-        fetchBMK();
-    }, []);
 
     return (
         <>
@@ -70,28 +45,15 @@ function HospitalView() {
                 <div className="mb-[24px]">
                     <div className="flex gap-[2px]8">
                         <div onClick={handleOnclick}>
-                            {bookmarkState == null ? (
-                                // 이거 누르면 create axios로 실행되게 해야함
-                                <div onClick={addBMK}>
-                                    <img src="/assets/images/bmkIcon_clear.svg" />
-                                </div>
-                            ) : (
+                            {bookmark ? (
                                 // 이거는 누르면 delete axios로 실행되게 해야함
-                                <div onClick={deleteBMK}>
+                                <div onClick={addBMK}>
                                     <img src="/assets/images/bmkIcon_color.svg" />
                                 </div>
+                            ) : (
+                                // 이거 누르면 create axios로 실행되게 해야함
+                                <img src="/assets/images/bmkIcon_clear.svg" />
                             )}
-                            {/* {bookmark ? (
-                                // 이거는 누르면 delete axios로 실행되게 해야함
-                                <div onClick={deleteBMK}>
-                                    <img src="/assets/images/bmkIcon_color.svg" />
-                                </div>
-                            ) : (
-                                // 이거 누르면 create axios로 실행되게 해야함
-                                <div onClick={addBMK}>
-                                    <img src="/assets/images/bmkIcon_clear.svg" />
-                                </div>
-                            )} */}
                         </div>
                         <p className="subtitle2">{hospitalInfo.name}</p>
                     </div>
