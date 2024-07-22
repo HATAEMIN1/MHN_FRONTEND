59c702fc1731fccf78dd96b3ac1bdd46732d62a2
HaTaeMin <gkxoals33@gmail.com>
2024-07-22 15:46:05 +0900

    결제 db저장전

--- src/pages/subscriptionPage/Payment.js
+++ src/pages/subscriptionPage/Payment.js
@@ -6,54 +6,74 @@ import axiosInstance from "../../utils/axios";
 import { useNavigate } from "react-router-dom";
 
 const { IMP } = window;
-// IMP.init("imp05251176"); // 'imp00000000' 대신 발급받은 가맹점 식별코드를 사용합니다.
 function Payment() {
     const navigate = useNavigate();
-    function onClickPayment() {
+    const pg = "kakaopay";
+    const merchantUid = generateRandomString("merchantUid");
+    const customerUid = generateRandomString("billingKey");
+    const userName = "포트원";
+    const tel = "02-1234-1234";
+    function generateRandomString(prefix) {
+        // 현재 시간을 밀리초 단위로 가져옵니다.
+        const timestamp = new Date().getTime();
+        // 랜덤한 16진수 문자열을 생성합니다.
+        const randomPart = Math.random().toString(16).substr(2, 8);
+        // prefix, timestamp, 랜덤 문자열을 조합합니다.
+        return `${prefix}_${timestamp}_${randomPart}`;
+    }
+    const currentDate = () => {
+        return new Date()
+            .toLocaleDateString("ko-KR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            })
+            .replace(/\. /g, ".")
+            .replace(/\.$/, "");
+    };
+
+    const nextPaymentDate = () => {
+        const d = new Date();
+        d.setMonth(d.getMonth() + 1);
+        return d
+            .toLocaleDateString("ko-KR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+            })
+            .replace(/\. /g, ".")
+            .replace(/\.$/, "");
+    };
+    const onClickPayment = () => {
         /* 1. 가맹점 식별하기 */
         const imp_uid = "imp05251176";
         IMP.init(imp_uid); //imp_uid
         /* 2. 결제 데이터 정의하기 */
         const data = {
-            pg: "kakaopay", // PG사 (필수)
-            merchant_uid: "order_monthly_12", // 주문번호 (필수)
-            name: "gkxoals33@gmail.com", //필수
-            amount: 8900, // 결제금액 (필수) 숫자타입이여야함
-            // customer_uid: "gkxoals33@gmail.com",
-            // buyer_name: "포트원", // 구매자 이름
-            buyer_tel: "02-1234-1234", // 구매자 전화번호
+            pg: pg, // PG사 (필수)
+            merchant_uid: merchantUid, //(주문번호) 빌링키 발급용 주문번호
+            customer_uid: customerUid, //(빌링키) 카드(빌링키)와 1:1로 대응하는 값  빌링키 <-이걸로 재 결제해야함
+            name: "구독 정기결제", //(상품 이름)
+            amount: 8900, // (결제금액)
+            buyer_name: userName, // (고객이름)
+            buyer_tel: tel, // (구매자 전화번호)
             // buyer_email: "gkxoals33@gmail.com", // 구매자 이메일
-            // m_redirect_url: "{모바일에서 결제 완료 후 리디렉션 될 URL}",
+            m_redirect_url: "http://localhost:3000/subscription/payment",
         };
-
         /* 4. 결제 창 호출하기 */
         IMP.request_pay(data, callback);
-        // IMP.request_pay(
-        //     {
-        //         pg: "kakaopay",
-        //         merchant_uid: "order_monthly_0001", // 상점에서 관리하는 주문 번호
-        //         name: "최초인증결제",
-        //         amount: 10, // 결제창에 표시될 금액. 실제 승인이 이뤄지지는 않습니다.
-        //         customer_uid: "your-customer-unique-id", // 필수 입력.
-        //         buyer_email: "test@portone.io",
-        //         buyer_name: "포트원",
-        //         buyer_tel: "02-1234-1234",
-        //
-        //     },
-        //     callback
-        // );
-    }
-
+    };
     /* 3. 콜백 함수 정의하기 */
-    function callback(response) {
+    const callback = (response) => {
         console.log("리스폰스값들: ");
         console.log(response);
         const {
-            success,
             pg_provider,
             paid_amount,
             merchant_uid,
+            customer_uid,
             name,
+            buyer_name,
             buyer_tel,
             imp_uid,
             error_msg,
@@ -63,46 +83,32 @@ function Payment() {
         // const imp_uid = response.imp_uid; //고유번호
         if (response.success) {
             console.log("결제 성공");
-            checkPayment(imp_uid, merchant_uid); //결제 검증
             paymentSubmit(
-                imp_uid,
                 pg_provider,
                 paid_amount,
                 merchant_uid,
+                customer_uid,
                 name,
-                buyer_tel
+                buyer_name,
+                buyer_tel,
+                imp_uid,
+                error_msg
             ); //결제내역 저장
+            ReservationPayment();
         } else {
             alert(`결제 실패: ${error_msg}`);
         }
-    }
-    //백엔드 검증 함수
-    const checkPayment = async (
-        imp_uid,
-        pg,
-        amount,
-        merchant_uid,
-        name,
-        buyer_tel
-    ) => {
-        try {
-            console.log("백엔드 검증 실행");
-            console.log("imp_uid는" + imp_uid);
-            const res = await axiosInstance.get("/verify/" + imp_uid);
-            console.log("결제 검증 완료", res.data);
-            //db에 저장
-            // pointSubmit(merchant_uid);
-        } catch (error) {
-            console.error("결제 검증 실패", error);
-        }
     };
     const paymentSubmit = async (
-        imp_uid,
         pg_provider,
         paid_amount,
         merchant_uid,
+        customer_uid,
         name,
-        buyer_tel
+        buyer_name,
+        buyer_tel,
+        imp_uid,
+        error_msg
     ) => {
         try {
             const body = {
@@ -110,16 +116,48 @@ function Payment() {
                 pg: pg_provider,
                 amount: paid_amount,
                 merchantUid: merchant_uid,
-                email: name,
-                buyer_tel,
+                customerUid: customer_uid,
+                productName: name,
+                buyerName: buyer_name,
+                buyerTel: buyer_tel,
+                nextBillingDate: nextPaymentDate(),
             };
             const res = await axiosInstance.post("/subscriptions", body);
             console.log(res.data, "디비저장완료");
-            navigate("/subscription/:userId");
+            // navigate("/subscription/:userId");
         } catch (e) {
             console.log("디비저장실패", e);
         }
     };
+    const ReservationPayment = async () => {
+        try {
+            const res = axiosInstance.get("/payments");
+
+            const billingKeyRequestData = {
+                schedules: [
+                    {
+                        merchant_uid: generateRandomString("Reservation"),
+                        schedule_at: Math.floor(Date.now() / 1000) + 86400, // 예: 24시간 후
+                        currency: "KRW",
+                        amount: 8900, // 예시 금액
+                        name: "정기결제 상품",
+                        buyer_name: userName,
+                        buyer_email: "gildong@example.com",
+                        buyer_tel: "010-1234-5678",
+                    },
+                ],
+            };
+
+            const response = await axiosInstance.post(
+                "/billing",
+                billingKeyRequestData
+            );
+            console.log("결제 예약 성공:", response.data);
+        } catch (error) {
+            console.error("결제 예약 실패:", error);
+        }
+    };
+
     return (
         <>
             <Header></Header>
@@ -127,8 +165,8 @@ function Payment() {
                 <div className="flex justify-between items-center p-4 border-b ">
                     <h2 className="subtitle1">주문 고객</h2>
                     <div className="flex">
-                        <p className="body2">김츄츄</p>
-                        <p className="body2">(010-1234-5678)</p>
+                        <p className="body2">{userName}</p>
+                        <p className="body2">{tel}</p>
                     </div>
                 </div>
                 <div>
@@ -160,9 +198,9 @@ function Payment() {
                 <div className="px-4 flex justify-between items-center">
                     <div className="subtitle2">혜택 기간</div>
                     <div className="flex">
-                        <p>2024.06.25</p>
+                        <p>{currentDate()}</p>
                         <p>~</p>
-                        <p>2024.07.25</p>
+                        <p>{nextPaymentDate()}</p>
                     </div>
                 </div>
                 <div className="p-4 font-semibold border-b pb-8">
@@ -176,7 +214,7 @@ function Payment() {
                 </div>
                 <div className="flex p-4 justify-between items-center border-b">
                     <div className="subtitle2">다음 결제 예정일</div>
-                    <p className="body2">2024.07.24</p>
+                    <p className="body2">{nextPaymentDate()}</p>
                 </div>
                 <div className="flex p-4 justify-between items-center border-b">
                     <div className="subtitle2">최종 결제 금액</div>
@@ -187,6 +225,13 @@ function Payment() {
                         위 내용을 확인하였으며, 멍햄냥 멤버십 가입에 동의합니다.
                     </p>
                 </div>
+                {/*<div*/}
+                {/*    onClick={() => {*/}
+                {/*        getAccessToken();*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    액세스 겟 토큰*/}
+                {/*</div>*/}
             </form>
             <ButtonBlack
                 text1="8,900원 정기 결제하기"
