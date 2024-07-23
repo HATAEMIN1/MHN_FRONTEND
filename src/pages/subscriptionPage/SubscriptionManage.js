2b4afe734512ca8ae1ce1ff8f231afbedb0a07f2
HaTaeMin <gkxoals33@gmail.com>
2024-07-23 17:54:32 +0900

    예약 결제 완료

--- src/pages/subscriptionPage/SubscriptionManage.js
+++ src/pages/subscriptionPage/SubscriptionManage.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../layouts/header/Header";
 import NavBar from "../../layouts/nav/NavBar";
 import ButtonBlack from "../../components/button/ButtonBlack";
+import { useParams } from "react-router-dom";
+import axiosInstance from "../../utils/axios";
 
 function SubscriptionManage(props) {
+    const statusMap = {
+        ACTIVE: "구독 중",
+        CANCELLED: "해지 중",
+    };
+    function getStatusText(status) {
+        return statusMap[status];
+    }
+    const { userId } = useParams();
+    const [mySubscription, setMySubscription] = useState("");
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        return `${year}.${month}.${day}`;
+    }
+    const mySubscribe = async () => {
+        const params = { userId };
+        const res = await axiosInstance.get("subscriptions", { params });
+        const formattedData = {
+            ...res.data,
+            paymentDate: formatDate(res.data.paymentDate),
+            nextBillingDate: formatDate(res.data.nextBillingDate),
+        };
+        setMySubscription(formattedData);
+        console.log(formattedData);
+    };
+    useEffect(() => {
+        mySubscribe();
+    }, []);
     return (
         <>
             <Header title="구독 내역"></Header>
@@ -12,15 +45,21 @@ function SubscriptionManage(props) {
                 전문가 1:1 채팅상담 + 진료 기록 내역 조회
             </div>
             <div className="p-4 flex justify-between items-center px-16 ">
-                <div className="subtitle2">구독중</div>
+                <div className="subtitle2">
+                    {mySubscription && getStatusText(mySubscription.status)}
+                </div>
                 <div>
                     <div className="flex gap-4 justify-between p-2">
                         <div className="body2">결제일</div>
-                        <div className="body2">2024.06.27</div>
+                        <div className="body2">
+                            {mySubscription && mySubscription.paymentDate}
+                        </div>
                     </div>
                     <div className="flex gap-4 justify-between p-2">
                         <div className="body2">만료일</div>
-                        <div className="body2">2024.07.27</div>
+                        <div className="body2">
+                            {mySubscription && mySubscription.nextBillingDate}
+                        </div>
                     </div>
                     <div className="flex gap-4 justify-between p-2">
                         <div className="body2">결제금액</div>
