25556b5abc131ceb796ba453c93f5741274863b9
ked1125 <ked1125@naver.com>
2024-07-23 20:50:40 +0900

    마이페이지 즐겨찾기 리스트 백엔드 연결

--- src/components/Form/HospitalListForm.js
+++ src/components/Form/HospitalListForm.js
@@ -10,21 +10,17 @@ function HospitalListForm({ ...props }) {
                         <div className="mb-[20px]">
                             <Link to={`/hospitals/${item.id}`}>
                                 <div className="flex items-center py-[10px] gap-[8px]">
-                                    <div>
+                                    <div className="w-[65px] h-[65px] flex-shrink-0">
                                         {/* <img src="여기에 나중에 프롭스해온 병원정보값을 이미지로 출력할 예정입니다." /> */}
                                         <img
                                             src="/assets/logoColor.png"
-                                            className="rounded-[4px] w-[65px] h-[65px]"
+                                            className="rounded-[4px] block w-full"
                                         />
                                     </div>
                                     <div>
                                         <p className="subtitle1">{item.name}</p>
                                         <p className="body2">{item.address}</p>
                                         <div className="flex gap-[4px]">
-                                            <p className="mini text-gray-300">
-                                                {/* {item.latitude} |{item.longitude} | */}
-                                                {item.distance} |
-                                            </p>
                                             <p className="mini text-gray-300">
                                                 {item.phone}
                                             </p>
