ac6c8d8ece4afeed635b672ddb5797dad4526c3a
ShinChangHun <tom951120@naver.com>
2024-07-29 17:48:05 +0900

    account

--- src/pages/boardPage/BoardList.js
+++ src/pages/boardPage/BoardList.js
@@ -5,7 +5,6 @@ import Searchbar from "../../components/search/Searchbar";
 import NavBar from "../../layouts/nav/NavBar";
 import FilterModalManager from "../../components/modal/FilterModalManager";
 import axiosInstance from "../../utils/axios";
-import { useSelector } from "react-redux";
 
 function timeSince(date) {
     const now = new Date();
@@ -18,15 +17,15 @@ function timeSince(date) {
         return `${Math.floor(secondsPast / 60)}분 전`;
     }
     if (secondsPast < 86400) {
-        return `${Math.floor(secondsPast / 3600)}시간 전`;
+        return `${Math.floor(secondsPast / 3600)}시간 전}`;
     }
     if (secondsPast < 2592000) {
-        return `${Math.floor(secondsPast / 86400)}일 전`;
+        return `${Math.floor(secondsPast / 86400)}일 전}`;
     }
     if (secondsPast < 31536000) {
-        return `${Math.floor(secondsPast / 2592000)}개월 전`;
+        return `${Math.floor(secondsPast / 2592000)}개월 전}`;
     }
-    return `${Math.floor(secondsPast / 31536000)}년 전`;
+    return `${Math.floor(secondsPast / 31536000)}년 전}`;
 }
 
 function BoardList() {
@@ -36,12 +35,12 @@ function BoardList() {
     const [currentPage, setCurrentPage] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
     const [pageRange, setPageRange] = useState([0, 4]);
-    const [filter, setFilter] = useState("latest"); // 수정된 부분: 필터 상태 추가
+    const [filter, setFilter] = useState("latest");
     const navigate = useNavigate();
 
     useEffect(() => {
         fetchPosts(currentPage, filter);
-    }, [currentPage, filter]); // 수정된 부분: 필터 상태가 변경될 때마다 API 호출
+    }, [currentPage, filter]);
 
     const fetchPosts = (page, filter) => {
         setLoading(true);
@@ -64,26 +63,20 @@ function BoardList() {
             });
     };
 
-    const handleModalOpen = () => {
-        console.log("모달 버튼 클릭됨");
-    };
-
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
 
     const handleNextRange = () => {
-        const newStart = pageRange[1] + 1;
+        const newStart = pageRange[0] + 1;
         const newEnd = Math.min(newStart + 4, totalPages - 1);
         setPageRange([newStart, newEnd]);
-        setCurrentPage(newStart);
     };
 
     const handlePrevRange = () => {
         const newEnd = pageRange[0] - 1;
         const newStart = Math.max(newEnd - 4, 0);
         setPageRange([newStart, newEnd]);
-        setCurrentPage(newStart);
     };
 
     const handleSearch = (query) => {
@@ -92,7 +85,7 @@ function BoardList() {
 
     const handleFilterChange = (newFilter) => {
         setFilter(newFilter);
-        setCurrentPage(0); // 필터 변경 시 페이지를 0으로 초기화
+        setCurrentPage(0);
     };
 
     if (loading) {
@@ -104,16 +97,15 @@ function BoardList() {
     }
 
     return (
-        <div className="w-full">
+        <div className="w-full flex flex-col min-h-screen">
             <Header title="자유게시판" write="boards/new" />
-            <div className="py-[16px] ">
+            <div className="py-[16px]">
                 <ul className="flex justify-around gap-[2px]">
-                    <Link to="/chatboards" className=" w-full">
-                        <li className=" text-center body2 border-b w-full">
+                    <Link to="/chatboards" className="w-full">
+                        <li className="text-center body2 border-b w-full">
                             1:1 채팅 게시판
                         </li>
                     </Link>
-
                     <li className="border-b-2 w-full text-center body2">
                         자유 게시판
                     </li>
@@ -123,10 +115,7 @@ function BoardList() {
                 <Searchbar onSearch={handleSearch} />
                 <FilterModalManager
                     modalOpen={
-                        <div
-                            onClick={handleModalOpen}
-                            style={{ cursor: "pointer" }}
-                        >
+                        <div onClick={() => {}} style={{ cursor: "pointer" }}>
                             <img
                                 src="/assets/images/filterIcon.svg"
                                 alt="Filter"
@@ -134,88 +123,93 @@ function BoardList() {
                             />
                         </div>
                     }
-                    onOpenModal={handleModalOpen}
-                    onFilterChange={handleFilterChange} // 수정된 부분: 필터 변경 함수 전달
+                    onFilterChange={handleFilterChange}
                 />
             </div>
-            <ul className="grid grid-cols-2 gap-2 w-full p-0 m-0 list-none">
-                {posts.map((post, index) => (
-                    <Link
-                        to={`/boards/${post.id}`}
-                        key={index}
-                        state={{ post }}
-                    >
-                        <li
+            <div className="flex-grow overflow-auto">
+                <ul className="grid grid-cols-2 gap-2 w-full p-0 m-0 list-none">
+                    {posts.map((post, index) => (
+                        <Link
+                            to={`/boards/${post.id}`}
                             key={index}
-                            className="w-full border border-gray-300 shadow-sm p-4 rounded-lg flex flex-col items-start h-64"
+                            state={{ post }}
                         >
-                            <div className="w-full h-40 mb-4 overflow-hidden shadow-md">
-                                {post.imageList &&
-                                    post.imageList.length > 0 && (
-                                        <img
-                                            src={`${process.env.REACT_APP_SPRING_SERVER_UPLOAD_URL}/upload/${post.imageList[0].fileName}`}
-                                            alt="Post Image"
-                                            className="w-full h-full object-cover"
-                                        />
-                                    )}
-                            </div>
-                            <div className="flex justify-between items-center w-full">
-                                <div className="font-bold text-base text-left text-black truncate">
-                                    {post.title}
+                            <li className="w-full border border-gray-300 shadow-sm p-4 rounded-lg flex flex-col items-start h-64">
+                                <div className="w-full h-40 mb-4 overflow-hidden shadow-md">
+                                    {post.imageList &&
+                                        post.imageList.length > 0 && (
+                                            <img
+                                                src={`${process.env.REACT_APP_SPRING_SERVER_UPLOAD_URL}/upload/${post.imageList[0].fileName}`}
+                                                alt="Post Image"
+                                                className="w-full h-full object-cover"
+                                            />
+                                        )}
                                 </div>
-                                <span className="text-sm text-gray-200 whitespace-nowrap">
-                                    {timeSince(post.createDate)}
-                                </span>
-                            </div>
-                            <p className="text-sm text-left overflow-hidden truncate">
-                                {post.content.substring(0, 10) +
-                                    (post.content.length > 10 ? "..." : "")}
-                            </p>
-                            <div className="flex gap-[4px]">
-                                <p className="mini text-gray-300">
-                                    좋아요 {post.likeCount}
+                                <div className="flex justify-between items-center w-full">
+                                    <div className="font-bold text-base text-left text-black truncate">
+                                        {post.title}
+                                    </div>
+                                    <span className="text-sm text-gray-200 whitespace-nowrap">
+                                        {timeSince(post.createDate)}
+                                    </span>
+                                </div>
+                                <p className="text-sm text-left overflow-hidden truncate">
+                                    {post.content.substring(0, 10) +
+                                        (post.content.length > 10 ? "..." : "")}
                                 </p>
-                                <p className="mini text-gray-300">|</p>
-                                <p className="mini text-gray-300">
-                                    댓글 {post.commentCount}
-                                </p>
-                            </div>
-                        </li>
-                    </Link>
-                ))}
-            </ul>
-            <div className="flex justify-center mt-4">
-                {pageRange[0] > 0 && (
-                    <button
-                        onClick={handlePrevRange}
-                        className="mx-1 px-3 py-1 border rounded bg-white"
-                    >
-                        &lt;
-                    </button>
-                )}
-                {Array.from(
-                    { length: pageRange[1] - pageRange[0] + 1 },
-                    (_, index) => {
-                        const pageIndex = pageRange[0] + index;
-                        return (
-                            <button
-                                key={pageIndex}
-                                onClick={() => handlePageChange(pageIndex)}
-                                className={`mx-1 px-3 py-1 border rounded ${currentPage === pageIndex ? "bg-gray-300" : "bg-white"}`}
-                            >
-                                {pageIndex + 1}
-                            </button>
-                        );
-                    }
-                )}
-                {pageRange[1] < totalPages - 1 && (
-                    <button
-                        onClick={handleNextRange}
-                        className="mx-1 px-3 py-1 border rounded bg-white"
-                    >
-                        &gt;
-                    </button>
-                )}
+                                <div className="flex gap-[4px]">
+                                    <p className="mini text-gray-300">
+                                        좋아요 {post.likeCount}
+                                    </p>
+                                    <p className="mini text-gray-300">|</p>
+                                    <p className="mini text-gray-300">
+                                        댓글 {post.commentCount}
+                                    </p>
+                                </div>
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className="sticky bottom-0 bg-white py-4 shadow-md">
+                <div className="flex justify-center">
+                    {pageRange[0] > 0 && (
+                        <button
+                            onClick={handlePrevRange}
+                            className="mx-1 px-3 py-1 border rounded bg-white"
+                        >
+                            &lt;
+                        </button>
+                    )}
+                    {Array.from(
+                        {
+                            length: Math.min(
+                                pageRange[1] - pageRange[0] + 1,
+                                totalPages - pageRange[0]
+                            ),
+                        },
+                        (_, index) => {
+                            const pageIndex = pageRange[0] + index;
+                            return (
+                                <button
+                                    key={pageIndex}
+                                    onClick={() => handlePageChange(pageIndex)}
+                                    className={`mx-1 px-3 py-1 border rounded ${currentPage === pageIndex ? "bg-gray-300" : "bg-white"}`}
+                                >
+                                    {pageIndex + 1}
+                                </button>
+                            );
+                        }
+                    )}
+                    {pageRange[1] < totalPages - 1 && (
+                        <button
+                            onClick={handleNextRange}
+                            className="mx-1 px-3 py-1 border rounded bg-white"
+                        >
+                            &gt;
+                        </button>
+                    )}
+                </div>
             </div>
             <NavBar />
         </div>
